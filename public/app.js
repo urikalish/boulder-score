428fd73974c9956ab8ef797acdac926994ec1ec5
kalish <ukalish@opentext.com>
2026-02-28 12:14:05 +0200

    wip

--- public/app.js
+++ public/app.js
@@ -4,49 +4,49 @@ const config = {
     fallPenalty: 10,
     levels: [
         {
-            v: 0,
-            points: 100,
-            problems: [1],
-        },
-        {
-            v: 1,
-            points: 200,
-            problems: [2, 3],
-        },
-        {
-            v: 2,
-            points: 300,
-            problems: [4, 5, 6, 7, 8],
-        },
-        {
-            v: 3,
-            points: 400,
-            problems: [9, 10, 11, 12, 13],
-        },
-        {
-            v: 4,
-            points: 500,
-            problems: [14, 15, 16, 17, 18],
-        },
-        {
-            v: 5,
-            points: 600,
-            problems: [19, 20, 21, 22, 23],
-        },
-        {
-            v: 6,
-            points: 700,
-            problems: [24, 25, 26, 27],
+            v: 8,
+            points: 900,
+            problems: [30],
         },
         {
             v: 7,
             points: 800,
-            problems: [28, 29],
+            problems: [29, 28],
         },
         {
-            v: 8,
-            points: 900,
-            problems: [30],
+            v: 6,
+            points: 700,
+            problems: [27, 26, 25, 24],
+        },
+        {
+            v: 5,
+            points: 600,
+            problems: [23, 22, 21, 20, 19],
+        },
+        {
+            v: 4,
+            points: 500,
+            problems: [18, 17, 16, 15, 14],
+        },
+        {
+            v: 3,
+            points: 400,
+            problems: [13, 12, 11, 10, 9],
+        },
+        {
+            v: 2,
+            points: 300,
+            problems: [8, 7, 6, 5, 4],
+        },
+        {
+            v: 1,
+            points: 200,
+            problems: [3, 2],
+        },
+        {
+            v: 0,
+            points: 100,
+            problems: [1],
         },
     ]
 };
